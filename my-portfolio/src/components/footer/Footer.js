7cab21bc4fe071348b9a66886fcacd294e01b0da
Mauri <dronm27a.z@gmail.com>
2021-10-04 16:59:57 -0300

    create Footer component and Add styles

--- my-portfolio/src/components/footer/Footer.js
+++ my-portfolio/src/components/footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {socialData} from '../data/SocialData';
 
+import style from './Footer.module.css';
+
 export default function Footer() {
     return(
         <ul>
-            {socialData?.map(data => {
+            {socialData?.map((data, i) => {
                 return (
-                    <li>
+                    <li key={i} className={style.lista}>
                         <a href={data.url}>
                             <img src={data.logo}  alt='' />    
                         </a>
