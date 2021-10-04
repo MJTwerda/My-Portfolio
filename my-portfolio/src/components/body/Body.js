35685656de4a6a7c4b635d9d1fb3d12c8f34ffd3
Mauri <dronm27a.z@gmail.com>
2021-10-04 17:00:33 -0300

    Add hr and br tags

--- my-portfolio/src/components/body/Body.js
+++ my-portfolio/src/components/body/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from '../about/About';
 import Footer from '../footer/Footer';
+import Skills from '../skills/Skills';
 
 import image from '../utils/images/Perfil.jpeg';
 
@@ -9,7 +10,10 @@ export default function Body() {
         <div>
             <img src={image} width='150' height='200' alt='' />
             <About />
+            <hr />
             <Footer />
+            <hr />
+            <Skills />
         </div>
     )
 }
