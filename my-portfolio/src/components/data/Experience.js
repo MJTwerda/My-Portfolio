477dff9216c07ef05e6ed0bac0555ccd7b2afd6e
Mauri <dronm27a.z@gmail.com>
2021-10-10 14:37:18 -0300

    Renderize tecnologies to projects

--- my-portfolio/src/components/data/Experience.js
+++ my-portfolio/src/components/data/Experience.js
@@ -3,6 +3,6 @@ export const experience = [
         company:require('../utils/images/logoTECHOceleste.png').default,
         image: require('../utils/images/experienceTECHO.png').default,
         url: 'https://www.linkedin.com/feed/update/urn:li:activity:6851362110262378496/',
-        tecnologies: ['TypeScript', 'React', 'PostegreSQL', 'Sequelize', 'Swagger', 'GitHub', 'Formik', 'MaterialUI', 'Material-Table'],
+        tecnologies: ['TypeScript', 'React', 'PostgreSQL', 'Sequelize', 'Swagger', 'GitHub', 'Formik', 'MaterialUI', 'Material-Table'],
     },
 ]
--- my-portfolio/src/components/data/ProjectsDetail.js
+++ my-portfolio/src/components/data/ProjectsDetail.js
@@ -4,13 +4,13 @@ export const projectsDetail = [
         description: '',
         url: 'https://www.linkedin.com/feed/update/urn:li:activity:6849431877179318272/',
         image: require('../utils/images/imgProMovies.png').default,
-        tecnologies: ['react', 'redux', 'css'],
+        tecnologies: ['React', 'Redux', 'CSS'],
     },
     {
         name: 'VideoGames Library',
         description: '',
         url: 'https://github.com/dronM27/Video-Games-Library',
         image: require('../utils/images/imgProGames.png').default,
-        tecnologies: ['react', 'redux', 'postgresql', 'express', 'css'],
+        tecnologies: ['React', 'Redux', 'PostgreSQL', 'Express', 'CSS'],
     },
 ]
