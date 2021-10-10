a1363479b8874587a3afffedacfb691ac46c558d
Mauri <dronm27a.z@gmail.com>
2021-10-10 16:36:51 -0300

    Add experience & styles

--- my-portfolio/src/components/data/Experience.js
+++ my-portfolio/src/components/data/Experience.js
@@ -1,8 +1,10 @@
 export const experience = [
     {
-        company:require('../utils/images/logoTECHOceleste.png').default,
+        company: require('../utils/images/logoTECHOceleste.png').default,
         image: require('../utils/images/experienceTECHO.png').default,
         url: 'https://www.linkedin.com/feed/update/urn:li:activity:6851362110262378496/',
         tecnologies: ['TypeScript', 'React', 'PostgreSQL', 'Sequelize', 'Swagger', 'GitHub', 'Formik', 'MaterialUI', 'Material-Table'],
+        date: '29/08/21 - 24/09/21 - Pasantía Estudiantil',
+        description: ['Optimización de código', 'Búsqueda, ordenamiento y filtrado de postulaciones / postulantes', 'Agilizar la selección y comunicación con candidatos para el voluntariado', 'Rechazar/Aceptar candidatos mediante un botón que actualice su estado en una Base de datos', 'Envío automático y personalizado de emails', 'Capacidad de entrevistar a candidatos para una posición', 'Creación / Visualización de datos personales'],
     },
-]
+];
--- my-portfolio/src/components/experience/Experience.js
+++ my-portfolio/src/components/experience/Experience.js
@@ -6,15 +6,26 @@ import style from './Experience.module.css';
 export default function Experience() {
     return (
         <div id='experience'>
-            <h2 className={style.title}>Profesional Experience</h2>
+            <h2 className={style.title}>Experiencia Laboral</h2>
                 {experience?.map((exp, i) => {
                     return (
                         <div key={i} className={style.individualProject}>
                             <a href={exp.url} className={style.urlProject} >
                                 <img src={exp.company} className={style.titleImg} alt=''/>
-                                
                             </a>
-
+                            <div>
+                                <p className={style.date}>{exp.date}</p>
+                                <p className={style.descriptions}>
+                                Participación de un equipo de desarrollo ágil con sprints de una semana presentando a un Product Owner avances sobre el desarrollo de los componentes de Administradores para seleccionar, y ordenar candidatos.<br />
+                                {exp.description?.map((func, i) => {
+                                    return (
+                                        <p key={i} className={style.itemsDescriptions}>
+                                            🔦 {func}
+                                        </p>
+                                    )}
+                                )}
+                                </p>
+                            </div>
                         </div>
                     )
                 })}
