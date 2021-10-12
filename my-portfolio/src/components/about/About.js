32e6146cbc871661282a63e7022050990ff73c60
Mauri <dronm27a.z@gmail.com>
2021-10-12 11:35:27 -0300

    Change presentation

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -6,7 +6,7 @@ export default function About({mode}) {
     return(
         <div id='about'>
             <h2 className={mode === 'light' ? style.title : style.titleDark}>
-                Mauri Twerda
+                Hola 👋🏼 Soy Mauri Twerda, desarrollador Full-Stack y este es mi portafolio
             </h2> 
         </div>
     )
