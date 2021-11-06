c8ffca5325e157c5ff8e593232961be5ff907914
unknown <mauriciotwerda@gmail.com>
2021-11-06 20:05:46 -0300

    Add modalGame

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+import {imageStack, mixCards} from '../data/Stack';
 
 import style from './About.module.css';
+const imgBack = require('../utils/icons/neutro.jpg').default;
+
+const styles = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    height: '80%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const deckCards = mixCards(imageStack);
 
 export default function About({mode}) {
+    const [open, setOpen] = useState(false)
+
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
     return(
         <div id='about'>
             <h2 className={mode === 'light' ? style.title : style.titleDark}>
-                Hola 👋🏼 Soy Mauri Twerda, desarrollador Full-Stack <br />
-                y este es mi portafolio
+                Hola 👋🏼 Soy Mauricio Twerda, desarrollador Full-Stack <br />
+                y este es mi portafolio <br />
+                Te invito a jugar antes de seguir avanzando por la página
             </h2> 
+            <Button variant='outlined' color='secondary' onClick={handleOpen}>Jugar</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={styles}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Adivina los pares correspondientes 
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Tenés 3 minutos?
+                    </Typography>
+                    <ImageList variant='masonry'  sx={{width: '100%', height: '100%'}} 
+                                cols={8} rows={1} >
+                        {deckCards.map((img, index) => (
+                            <div key={index} className={style.containerCard}>
+                               <img
+                                    src={img.icon}
+                                    alt=''
+                                    loading="lazy"
+                                    className={style.cardBack}
+                                />
+                                <img 
+                                    src={imgBack}
+                                    alt=''
+                                    loading="lazy"
+                                    className={style.cardFront}
+                                />
+                            </div>
+                        ))}
+                    </ImageList>
+                </Box>
+            </Modal>
         </div>
     )
 }
--- my-portfolio/src/components/data/Stack.js
+++ my-portfolio/src/components/data/Stack.js
@@ -58,3 +58,66 @@ export const stack = [
         ]
     },
 ]
+
+export const imageStack = [
+    {
+        icon: require('../utils/icons/javascript-original.png').default,
+        position: 0
+    },
+    {
+        icon: require('../utils/icons/html-original.png').default,
+        position: 1
+    },
+    {
+        icon: require('../utils/icons/css-original.png').default,
+        position: 2
+    },
+    {
+        icon: require('../utils/icons/react-original.png').default,
+        position: 3
+    },
+    {
+        icon: require('../utils/icons/redux-original.png').default,
+        position: 4
+    },
+    {
+        icon: require('../utils/icons/typescript-original.png').default,
+        position: 5
+    },
+    {
+        icon: require('../utils/icons/nodejs-original.png').default,
+        position: 6
+    },
+    {
+        icon: require('../utils/icons/postgresql-original.png').default,
+        position: 7
+    },
+    {
+        icon: require('../utils/icons/socialIcons/github-logos1.png').default
+    },
+    {
+        icon: require('../utils/icons/sequelize-original.png').default,
+        position: 8
+    },
+    {
+        icon: require('../utils/icons/postman-original.png').default,
+        position: 9
+    },
+    {
+        icon: require('../utils/icons/swagger-original.png').default,
+        position: 10
+    },
+]
+
+export const mixCards = (cards) => {
+    let cardOnGame = [];
+
+    for (let i = 0; i < cards.length; i++) {
+        cardOnGame.push({icon: cards[i].icon, position: cards[i].position})
+        cardOnGame.unshift({icon: cards[i].icon, position: cards[i].position})
+    }
+    cardOnGame.forEach(card => {
+        card.position = Math.random();
+    })
+    return cardOnGame.sort((a, b) => a.position - b.position);
+}
