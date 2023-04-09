d9a1e63cc20bf865b6050ebc8b6f5a71ecfdf93b
TwerdaMJ <mtwerda@revaicare.com>
2023-04-08 23:43:29 -0300

    Se corrigen importaciones de archivos

--- src/components/data/ProjectsDetail.js
+++ src/components/data/ProjectsDetail.js
@@ -3,14 +3,14 @@ export const projectsDetail = [
         name: 'Movies Library',
         description: '',
         url: 'https://www.linkedin.com/feed/update/urn:li:activity:6849431877179318272/',
-        image: require('../utils/images/imgProMovies.png').default,
+        image: require('../../utils/images/imgProMovies.png').default,
         tecnologies: ['React', 'Redux', 'CSS'],
     },
     {
         name: 'VideoGames Library',
         description: '',
         url: 'https://github.com/dronM27/Video-Games-Library',
-        image: require('../utils/images/imgProGames.png').default,
+        image: require('../../utils/images/imgProGames.png').default,
         tecnologies: ['React', 'Redux', 'PostgreSQL', 'Express', 'CSS'],
     },
 ]
