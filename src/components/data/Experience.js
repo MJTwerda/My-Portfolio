39c2b73a9ebc84850eb3469fdb3764312f449ef8
tresDobleces <tresdobleces.desarrollo@gmail.com>
2024-12-23 18:33:08 -0300

    ADD Experience images

--- src/components/data/Experience.js
+++ src/components/data/Experience.js
@@ -1,6 +1,58 @@
 import logoTECHOceleste from '../../utils/images/logoTECHOceleste.png';
+import logoREVAI from '../../utils/images/revaiImage.jpg';
+import logoQUARES from '../../utils/images/quaresImage.jpeg';
 
 export const experience = [
+    {
+        company: logoQUARES,
+        url: 'https://www.quaresitsolutions.com/',
+        tecnologies: [
+            'TypeScript',
+            'React',
+            'Ant Design',
+            'Angular v8 y v12',
+            'Git',
+        ],
+        date: '16/04/24 - 05/11/24 - Desarrollador Web de múltiples aplicaciones',
+        description: [
+            'Optimización de código', 
+            'Participación en los procesos de diseño y desarrollo de distintas aplicaciones web', 
+            'Capacitación continua',
+            'Trato directo con clientes',
+            'Desarrollo mediante Metodologías Ágiles',
+            'Código escalable, seguro y reutilizable',
+        ]
+    },
+    {
+        company: logoREVAI,
+        url: 'https://www.linkedin.com/company/revaicare/',
+        tecnologies: [
+            'TypeScript',
+            'React',
+            'Redux',
+            'Material UI',
+            'Jest',
+            'Formik',
+            'Microfrontends',
+            'Git',
+            'NPM',
+            'NodeJS',
+            'Docker',
+            'Microservicios',
+        ],
+        date: '01/12/21 - 04/004/24 - Desarrollador Web',
+        description: [
+            'Optimización de código', 
+            'Participación en los procesos de diseño y desarrollo de la App', 
+            'Desarrollo mediante Metodologías Ágiles',
+            'Desarrollo de microfrontends y microservicios',
+            'Código escalable, seguro y reutilizable', 
+            'Agilizar el proceso de gestión de turnos de distintas compañías y sucursales médicas', 
+            'Búsqueda, filtrado y ordenamiento de turnos médicos, pacientes, flujos de trabajo, etc.', 
+            'Test unitarios', 
+            'Creación de librerías propias reutilizables para lograr distintas funcionalidades en la App'
+        ],
+    },
     {
         company: logoTECHOceleste,
         url: 'https://www.youtube.com/watch?v=GMjGTJrUBJo&t=5s&ab_channel=LuizferColorado',
@@ -8,12 +60,12 @@ export const experience = [
             'TypeScript', 
             'React', 
             'PostgreSQL',
-             'Sequelize',
-             'Swagger',
-             'GitHub',
-             'Formik',
-             'MaterialUI',
-             'Material-Table'
+            'Sequelize',
+            'Swagger',
+            'GitHub',
+            'Formik',
+            'MaterialUI',
+            'Material-Table'
         ],
         date: '29/08/21 - 24/09/21 - Pasantía Estudiantil',
         description: [
